ddeade731b2f22351489f0b7c1c213b08d003b61
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2020-11-09 21:36:03 -0600

    updating questions

--- Unsolved/index.js
+++ Unsolved/index.js
@@ -1,16 +1,65 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your Github username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'What is your projects name?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a small description of your project',
+    },
+    {
+      type: 'checkbox',
+      name: 'license',
+      message: 'What kind of license should your project have?',
+      choices: ['MIT', 'Apache 2.0', 'GLP 3.0', 'BDS 3.0', 'None'  ],
+    },
+    {
+      type: 'input',
+      name: 'dependencies',
+      message: 'What command should be run to install dependencies?',
+    },
+    {
+        type: 'input',
+        name: 'runTests',
+        message: 'What command should be run to run tests?',
+      },
+      {
+        type: 'input',
+        name: 'repo',
+        message: 'What does the user need to know about using the repo?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+      },
+  ]);
 
-// function to initialize program
-function init() {
+const generateREADME = (answers) =>
+`
+XXXXXXXXXXXX Put readme here XXXXXXXXXXXX
+`;
 
-}
-
-// function call to initialize program
-init();
+promptUser()
+  .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+  .then(() => console.log('Successfully wrote to README.md'))
+  .catch((err) => console.error(err));
